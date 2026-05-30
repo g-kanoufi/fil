82734eab6e613dd71921de7cadd5187417f6def7
Guillaume Kanoufi <g.kanoufi@gmail.com>
2026-05-30 14:54:07 +0200

    Rebuild frontend assets after activity export feature.

---
+++ backend/public/fil-assets/assets/areas-Drw671oY.js
@@ -0,0 +1,1 @@
+import{R as e}from"./main-Cd-c79iA.js";function t(){return e(`/v1/areas`).then(e=>e.data)}export{t};
--- backend/public/fil-assets/assets/areas-HZcc8Ykf.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./main-BK6moTlc.js";function t(){return e(`/v1/areas`).then(e=>e.data)}export{t};
